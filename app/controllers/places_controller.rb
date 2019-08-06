3ca7c8af4955351340565bece30d1e4d2cde6f88
sheard93 <shelbyzaglin@gmail.com>
2019-08-06 15:26:57 +0000

    Adding paginate

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,9 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
+
+  def index
+    @places = Place.all.order("created_at DESC").paginate(page: params[:page], per_page: 5)
+  end
+
 end
