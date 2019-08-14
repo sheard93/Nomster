dfe03355fd83bb50dcc309317358655cf7a7436c
sheard93 <shelbyzaglin@gmail.com>
2019-08-14 19:30:09 +0000

    Connected Users to Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,6 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @places = Place.all
   end
@@ -12,14 +14,14 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
-private
+  private
 
-def place_params
-  params.require(:place).permit(:name, :description, :address)
-end
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 
 end
