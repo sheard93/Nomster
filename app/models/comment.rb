ec21c4a6157fd33531f0581e851513eff0adc4f2
sheard93 <shelbyzaglin@gmail.com>
2019-09-04 16:25:12 +0000

    Moved comment form to modal

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,11 +3,11 @@ class Comment < ApplicationRecord
   belongs_to :place
 
   RATINGS = {
-    'one star': '1_star'
-    'two stars': '2_stars'
-    'three stars': '3_stars'
-    'four stars': '4_stars'
-    'five stars': '5_stars'
+    'one star': '1_star',
+    'two stars': '2_stars',
+    'three stars': '3_stars',
+    'four stars': '4_stars',
+    'five stars': '5_stars',
   }
   def humanized_rating
     RATINGS.invert[self.rating]
