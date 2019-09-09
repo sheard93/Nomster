ca1f39bc2bb69488f66dbeddfaf41f1ab5e51be7
sheard93 <shelbyzaglin@gmail.com>
2019-09-09 15:33:46 +0000

    moved code from application to development

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Nomster
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.web_console.permissions = '0.0.0.0'
+    
 
 
     # Settings in config/environments/* take precedence over those specified here.
